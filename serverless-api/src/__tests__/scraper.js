b8de7227c95c6b67eb17a043de44de46328da39e
Chris Cooney <chris_cooney@hotmail.co.uk>
2017-11-24 20:13:52 +0000

    Simple test. Simple functionality. Time to use tests to drive out

--- serverless-api/src/__tests__/scraper.js
+++ serverless-api/src/__tests__/scraper.js
@@ -1,13 +1,20 @@
 import scraper from '../scraper'
 import fs from 'fs'
 
+const mockRssHtml = fs.readFileSync(__dirname + '/resources/test-rss-sample.rss')
+
+jest.mock('../network', () => (
+  {
+    get: () => (
+      new Promise((resolve) => resolve({ body: mockRssHtml }))
+    )
+  }
+))
+
 describe('Test the scrapers ability to parse and return content', () => {
-
-  const validRssHtml = fs.readFileSync('./resources/test-rss-sample.rss')
-
   it('should parse and return correct objects when provided with correct html', () => {
-    /*
-      Need to be able to mock out the thing this is requesting.
-    */
+    return scraper.fetchArticleSummary().then((summaries) => {
+      expect(summaries.length).toBe(1)
+    })
   })
 })
--- serverless-api/src/scraper.js
+++ serverless-api/src/scraper.js
@@ -1,5 +1,13 @@
-const scraper = () => {
+import network from './network'
 
+const LAD_BIBLE_RSS_FEED = 'http://www.ladbible.com/news.rss'
+
+const fetchArticleSummary = () => (
+  network.get(LAD_BIBLE_RSS_FEED).then(() => {
+    return [1]
+  })
+)
+
+export default {
+  fetchArticleSummary,
 }
-
-export default scraper
