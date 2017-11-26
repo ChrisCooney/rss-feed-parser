8ba0bfe38cf761d77ac3269f2d09b3275efcf753
Chris Cooney <chris_cooney@hotmail.co.uk>
2017-11-26 13:16:06 +0000

    Made start up script a little smarter. Will default to npm if yarn can't be found

--- startup.sh
+++ startup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+echo "Checking is yarn is installed on this machine."
 echo "Starting up the client application. Server side is already deployed."
+
 cd client/
-echo "Installing client side dependencies"
-yarn
-echo "Dependencies installed. Starting up webpack dev server"
-yarn start
+
+if [ -x "$(command -v yarn)" ]; then
+  echo "Yarn detected. Using Yarn"
+  echo "Installing client side dependencies"
+  yarn
+  echo "Dependencies installed. Starting up webpack dev server"
+  yarn start
+else
+  echo "Can not find yarn. Defaulting to NPM"
+  echo "Installing client side dependencies"
+  npm i
+  echo "Dependencies installed. Starting up webpack dev server"
+  npm run start
+fi
