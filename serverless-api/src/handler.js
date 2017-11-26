e0759738614415895d6f624f3ba2fe9930aaff27
Chris Cooney <chris_cooney@hotmail.co.uk>
2017-11-26 11:51:50 +0000

    Moved headers out into their own object

--- serverless-api/src/handler.js
+++ serverless-api/src/handler.js
@@ -1,24 +1,23 @@
 import scraper from './scraper'
 
+const HEADERS = {
+  'Content-Type': 'application/json; charset=utf-8',
+  'Access-Control-Allow-Origin': '*',
+}
+
 export const images = (event, context, callback) => (
   scraper.fetchArticleSummaries()
     .then(summaries => {
       callback(null, {
         statusCode: 200,
         body: JSON.stringify(summaries),
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8',
-          'Access-Control-Allow-Origin': '*',
-        }
+        headers: HEADERS,
       })
     })
     .catch(() => {
       callback(null, {
         statusCode: 500,
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8',
-          'Access-Control-Allow-Origin': '*',
-        }
+        headers: HEADERS,
       })
     })
 )
